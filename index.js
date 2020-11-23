5d88fc304df0c5034dd545b2d131dc833bec8a03
ItsMeAndrea <itsandrea_94@hotmail.com>
2020-11-22 22:18:57 -0400

    test

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+let port = process.env.PORT || 5000;
+
+dotenv.config();
+const usersRoute = require("./app/routes/usersRoute");
+const leaderboardRoute = require("./app/routes/leaderboardRoute");
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//ROUTES
+app.use("/api/v1", usersRoute);
+app.use("/api/v1", leaderboardRoute);
+
+app.listen(port).on("listening", () => {
+  console.log(`live from ${port}`);
+});
+
+//prueba
